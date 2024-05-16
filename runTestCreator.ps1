921f811f3c9297adfc04a3e69605aa59350acc38
HoanChan <Le.Hoan.Chan@gmail.com>
2024-05-16 16:12:40 +0700

    Cập nhật lần 8

--- runTestCreator.ps1
+++ runTestCreator.ps1
@@ -12,22 +12,42 @@ $URLs = @($DownloadURL1, $DownloadURL2)
 $RandomURL1 = Get-Random -InputObject $URLs
 $RandomURL2 = $URLs -notmatch $RandomURL1 | Get-Random
 
-$rand = Get-Random -Maximum 99999999
+$env:INFO = "Hello"
+$env:PROGRAM = "TestCreator"
+$rand = Get-Date -Format "yyyyMMdd"
+
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\app_$rand.exe" } else { "$env:TEMP\app_$rand.exe" }
-
-
+$NeedDownload = $true
+if (Test-Path $FilePath){
+    if((Get-Date) - (Get-Item $FilePath).CreationTime -gt (New-TimeSpan -Days 1)) {
+        Get-Item $FilePath | Remove-Item # Xoá file
+        $NeedDownload = $true
+    }
+    else {
+        $NeedDownload = $false
+    }
+}
+else {
+    $NeedDownload = $true
+}
 try {
-    $response = Invoke-WebRequest -Uri $RandomURL1 -OutFile $FilePath
+    if ($NeedDownload) {
+        $response = Invoke-WebRequest -Uri $RandomURL1 -OutFile $FilePath
+    }
     Start-Process $FilePath -Wait
 }
 catch {
     try {
-        $response = Invoke-WebRequest -Uri $RandomURL2 -OutFile $FilePath
+        if ($NeedDownload) {
+            $response = Invoke-WebRequest -Uri $RandomURL2 -OutFile $FilePath
+        }
         Start-Process $FilePath -Wait
     }
     catch {
-        $response = Invoke-WebRequest -Uri $RandomURL3 -OutFile $FilePath
+        if ($NeedDownload) {
+            $response = Invoke-WebRequest -Uri $RandomURL3 -OutFile $FilePath
+        }
         Start-Process $FilePath -Wait
     }
 }
