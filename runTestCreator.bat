5633ee839af4512916869bd6d27c58c5f7c59b3c
HoanChan <Le.Hoan.Chan@gmail.com>
2024-12-22 10:43:19 +0700

    Sửa chút, quên bỏ chú thích

--- runTestCreator.bat
+++ runTestCreator.bat
@@ -2,13 +2,15 @@
 mkdir C:\TestCreator
 
 REM Tải xuống tệp PowerShell
-@REM powershell -Command "Invoke-WebRequest -Uri 'https://github.com/HoanChan/download/raw/main/runTestCreator.ps1' -OutFile 'C:\TestCreator\runTestCreator.ps1' -UseBasicP"
+@echo Dơnload PowerShell file
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/HoanChan/download/raw/main/runTestCreator.ps1' -OutFile 'C:\TestCreator\runTestCreator.ps1' -UseBasicP"
 
 REM Chạy tệp PowerShell với quyền admin
-@REM powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File C:\TestCreator\runTestCreator.ps1' -Verb RunAs"
+@echo Run PowerShell file
+powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File C:\TestCreator\runTestCreator.ps1' -Verb RunAs"
 
 REM Đếm ngược 15 giây
-echo Exit in
+echo Done! Exit in
 for /L %%i in (15,-1,1) do (
     echo %%i
     timeout /t 1 >nul
